8f1e08e3964e767dfd791a5a144f6ccf5c33c1ff
Andrew <andrew.j.heinke@gmail.com>
2016-07-26 19:00:29 -0500

    COMMENT

--- schema.sql
+++ schema.sql
@@ -1,13 +1,12 @@
-/* 
+/*
 This file is intended to help developers get their SQL Databases setup correctly.
-It is important since other developers will be unlikely to have the same database or tables setup already. 
+It is important since other developers will be unlikely to have the same database or tables setup already.
 */
 
 /* Create and use the starwars db */
 CREATE DATABASE  `starwars`;
 USE `starwars`;
 
-/* Create a table for all your star wars characters */
 CREATE TABLE `allcharacters` (
 	`id` Int( 11 ) AUTO_INCREMENT NOT NULL,
 	`routeName` VARCHAR( 255) NOT NULL,
@@ -16,4 +15,4 @@ CREATE TABLE `allcharacters` (
 	`age` Int(11) NOT NULL,
 	`forcePoints` Int(11) NOT NULL,
 
-	PRIMARY KEY ( `id` ) ); /* Set ID as primary key */
+	PRIMARY KEY ( `id` ) );
